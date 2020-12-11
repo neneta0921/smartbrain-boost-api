1fe1290bbff21a5248598afc626c2cae409d7c12
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-11 16:50:14 +0900

    npm i bcrypt-nodejs

--- server.js
+++ server.js
@@ -86,13 +86,3 @@ app.put('./image', (req, res) => {
 app.listen(3000, () => {
   console.log('app is running on port 3000')
 })
-
-
-/*
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user
-
-*/
