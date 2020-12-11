1b3a25db5c490369746a5f5c297cb8d498d52c81
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-11 14:52:43 +0900

    create /signin and test using POSTMAN

--- server.js
+++ server.js
@@ -2,8 +2,42 @@ const express = require('express');
 
 const app = express();
 
+// app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+const database = {
+  users: [
+    {
+      id: 123,
+      name: 'Dan',
+      email: 'dan@gmail.com',
+      password: 'cooking',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: 130,
+      name: 'Jane',
+      email: 'jane@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+// console.log(database.users[0].email, database.users[0].password)
+
 app.get('/', (req, res) => {
   res.send('this is working')
+})
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === database.users[0].email &&
+      req.body.password === database.users[0].password) {
+        res.json('success');
+  } else {
+    res.status(400).json('error logging in');
+  }
 })
 
 app.listen(3000, () => {
