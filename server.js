69ea1c1124797fcd4fb7cb872d26562d89856af4
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-20 10:14:19 +0900

    add body-parser

--- server.js
+++ server.js
@@ -1,48 +1,28 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
 const knex = require('knex');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const config = require('./config');
 const DATABASE_URL = config.restfulApiConfig.databaseURL;
-const DATABASE_HOST = config.restfulApiConfig.databaseHost;
-const DATABASE_USER = config.restfulApiConfig.databaseUser;
-const DATABASE_PASSWORD = config.restfulApiConfig.databasePassword;
-const DATABASE_NAME = config.restfulApiConfig.databaseName;
-
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-// const db = knex({
-//   client: 'pg',
-//   connection: {
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//       rejectUnauthorized: false
-//     }
-//   }
-// });
-
 const db = knex({
   client: 'pg',
   connection: process.env.POSTGRES_URI
-  // connection: {
-  //   host : DATABASE_HOST,
-  //   user : DATABASE_USER,
-  //   password : DATABASE_PASSWORD,
-  //   database : DATABASE_NAME
-  // }
 });
 
 const app = express();
 
-console.log(bcrypt.hashSync('123'))
-app.use(morgan('combined'))
 app.use(cors());
+app.use(bodyParser.json())
+app.use(morgan('combined'))
 app.use(express.json());
 
 app.get('/', (req, res) => { res.send('OK, it is working!') })
